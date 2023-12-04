c31311b1149bb2b4698be8c2a4d7aa3ee354fc1a
Guilherme <guilhermesilba811@gmail.com>
2023-12-04 10:51:32 -0300

    logo image recolocada

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import { useState } from "react";
 
 const Header = () => (
   <header className="header">
-    <img className="logo" src="./images/logo-espaco-mulher.png" alt="logo" />
+    <img
+      className="logo"
+      src="https://github.com/Gui089/Espaco-Mulher/blob/main/images/logo-espaco-mulher.png?raw=true"
+      alt="logo"
+    />
     <h1>Espaço Mulher</h1>
   </header>
 );
